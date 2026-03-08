8590ce682f53b5021a4230982131161bc318be36
EemmPaTECH-empire <EemmPaTECH@gmail.com>
2026-03-08 22:54:27 +0100

    Create gitignore/firebase-config.js

---
+++ gitignore/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCh0t_yRYsMUyNbgTFofibsJKEQz-bHIqE",
+  authDomain: "wavex-chat.firebaseapp.com",
+  projectId: "wavex-chat",
+  storageBucket: "wavex-chat.firebasestorage.app",
+  messagingSenderId: "674774241938",
+  appId: "1:674774241938:web:60dcecf424dd6d154dcc4b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
