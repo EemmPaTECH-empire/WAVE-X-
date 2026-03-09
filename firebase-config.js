7a67e571513950d4611718b3bccff7487dd1e7ac
EemmPaTECH-empire <EemmPaTECH@gmail.com>
2026-03-09 01:48:18 +0100

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyCh0t_yRYsMUyNbgTFofibsJKEQz-bHIqE",
-  authDomain: "wavex-chat.firebaseapp.com",
-  projectId: "wavex-chat",
-  storageBucket: "wavex-chat.firebasestorage.app",
-  messagingSenderId: "674774241938",
-  appId: "1:674774241938:web:60dcecf424dd6d154dcc4b"
-};
